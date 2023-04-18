379194de9310e9fbbc72b90bc629c3cd1002d7cf
Blazheiko <40579911+Blazheiko@users.noreply.github.com>
2023-04-18 18:36:32 +0300

    add debugger 9

--- src/server.ts
+++ src/server.ts
@@ -214,7 +214,7 @@ export class Server {
         metrics: {
             enabled: true,
             driver: 'prometheus',
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             prometheus: {
                 prefix: 'soketi_',
             },
